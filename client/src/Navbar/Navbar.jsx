dc849b16aeb64df98e00fdbd7f9a21e253f269d0
Rishav Paudel <tppaudel2018@gmail.com>
2025-05-28 03:49:28 +0545

    Navbar making, still left to edit the assets for Navbar

--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import './Navbar.css'
 
 import cart_icon from '../assets/cart-logo.png'
+import logo from '../assets/nav-logo.png'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-    
+    <div className='Navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt='F1boxxd logo'></img>
+        <p>F1Boxxd</p>
+      </div>
+      <ul className='nav-menu'>
+        <li>Clothes<hr></hr></li>
+        <li>Car Models<hr></hr></li>
+        <li>Engine Models<hr></hr></li>
+        <li>Posters<hr></hr></li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart_icon"></img>   
+      </div>
+    </div>
+   
   )
 }
 
