2b7bb5a91b2f3a274a81d573fa97d283a3030d6c
Rishav Paudel <tppaudel2018@gmail.com>
2025-05-28 01:57:52 +0545

    Getting the assets together for Navbar

---
+++ client/src/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+import cart_icon from '../assets/cart-logo.png'
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+    
+  )
+}
+
+export default Navbar
