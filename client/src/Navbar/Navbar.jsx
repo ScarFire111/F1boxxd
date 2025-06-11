07ac0075cfb534baaa2b2e62e2fcd927828ac584
Rishav Paudel <tppaudel2018@gmail.com>
2025-06-11 21:00:40 +0545

    Navbar adds

--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 import cart_icon from '../assets/cart-logo.png'
 import logo from '../assets/nav-logo.png'
+import cart_logo from '../Navbar/f1.png'
 const Navbar = () => {
+
+  const [menu,setMenu]=useState("shop");
   return (
     <div className='Navbar'>
       <div className='nav-logo'>
         <img src={logo} alt='F1boxxd logo'></img>
-        <p>F1Boxxd</p>
+       <div id="logo"> <span id="F1"><img src={cart_logo} alt="WebsiteLogo"></img></span>
+        </div>
       </div>
       <ul className='nav-menu'>
-        <li>Clothes<hr></hr></li>
-        <li>Car Models<hr></hr></li>
-        <li>Engine Models<hr></hr></li>
-        <li>Posters<hr></hr></li>
+        <li onClick={()=>setMenu('Clothes')}>Clothes<hr></hr></li>
+        <li onClick={()=>setMenu('Car_Models')}>Car Models<hr></hr></li>
+        <li onClick={()=>setMenu('Engine_Models')}>Engine Models<hr></hr></li>
+        <li onClick={()=>setMenu('Posters')}>Posters<hr></hr></li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="cart_icon"></img>   
+         <div className='nav-cart-count'>0</div>
       </div>
+     
     </div>
    
   )
