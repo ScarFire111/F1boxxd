5aa6c81f6818576d30673d84ee4adfa6234465dd
Rishav Paudel <tppaudel2018@gmail.com>
2025-06-30 18:55:44 +0545

    Hero page edits

--- client/src/Hero/Hero.jsx
+++ client/src/Hero/Hero.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import './Hero.css'
-import Michael_photo from '../assets/MichaelSchumacher.png'
-import F2004 from '../assets/F2004.png'
+import legendsart from '../assets/legendsart.png'
 
 export const Hero = () => {
   return (
     <div className='hero'>
         <div className="hero-left">
-          <h2>Michael Schumacher Special</h2>
-          <p>new<img src={F2004} alt="" className="F2004"></img></p>
-          <p>collections for</p>
-          <p>everyone</p>
+          <h2>Michael Schumacher edition with many more classics!</h2>
+          <p>We will help you</p>
+          <p>Choose the best for yourself</p>
 
           <div className="hero-latest-button">See-More</div>
+         
         </div>
         <div className="hero-right">
            <div className='hero-hand-icon'>
-              <img src={Michael_photo} alt="" />
+            <img src={legendsart} alt=" "></img>
             </div>  
         </div>
         
