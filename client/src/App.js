75c8221b5500187b4bcf1cd9068a0464d16fdc3d
Rishav Paudel <tppaudel2018@gmail.com>
2025-07-20 23:56:25 +0545

    Everything sorted

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,10 @@ import LoginSignUp from './Pages/LoginSignUp';
 import Homepage from './Pages/Homepage';
 import SignUp from './Pages/SignUp';
 import Footer from './Footer/Footer'
+import clothes_banner from './assets/Frontend_Assets/banner_mens.png'
+import carModels_banner from './assets/Frontend_Assets/banner_women.png'
+import engineModels_banner from './assets/Frontend_Assets/banner_kids.png'
+
 function App() {
   return (
     <div className="App">
@@ -15,10 +19,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Homepage/>} />
-          <Route path='/clothes' element={<ShopCategory category="Clothes" />} />
-          <Route path='/car_Models' element={<ShopCategory category="Car_Models" />} />
-          <Route path='/engine_Models' element={<ShopCategory category="Engine_Models" />} />
-          <Route path='/posters' element={<ShopCategory category="Posters" />} />
+          <Route path='/clothes' element={<ShopCategory banner={clothes_banner} category="clothes" />} />
+          <Route path='/car_Models' element={<ShopCategory banner={carModels_banner} category="car_Models" />} />
+          <Route path='/engine_Models' element={<ShopCategory banner={engineModels_banner} category="engine_Models" />} />
+          <Route path='/posters' element={<ShopCategory banner={clothes_banner} category="posters" />} />
           <Route path='/product/:ProductId' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/loginsignup' element={<LoginSignUp />} />
