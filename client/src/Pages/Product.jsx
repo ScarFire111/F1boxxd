115abd64ca7bc256967cc9fa24411f190e0f53b2
Rishav Paudel <tppaudel2018@gmail.com>
2025-07-21 01:00:39 +0545

    Product link to their id and a new video progress

--- client/src/Pages/Product.jsx
+++ client/src/Pages/Product.jsx
@@ -1,13 +1,16 @@
 import React,{useContext} from 'react'
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
+import { Breadcrums } from '../Breadcrums/Breadcrums';
 
 const Product = () => {
   const {all_product}=useContext(ShopContext);
   const {productId}=useParams();
   const product=all_product.find((e)=>e.id===Number(productId))
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrums product={product}/>
+    </div>
   )
 }
 export default Product;
