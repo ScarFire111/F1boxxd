ce7678010661a570c495813ac0bba1bec89ace10
Rishav Paudel <tppaudel2018@gmail.com>
2025-07-28 02:05:59 +0545

    Admin panel work

---
+++ admin/src/assets/Frontend_Assets/all_product.js
@@ -0,0 +1,329 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.jpg";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p16_img from "./product_16.png";
+import p17_img from "./product_17.png";
+import p18_img from "./product_18.jpg";
+import p19_img from "./product_19.jpg";
+import p20_img from "./product_20.png";
+import p21_img from "./product_21.png";
+import p22_img from "./product_22.png";
+import p23_img from "./product_23.png";
+import p24_img from "./product_24.png";
+import p25_img from "./product_25.png";
+import p26_img from "./product_26.png";
+import p27_img from "./product_27.png";
+import p28_img from "./product_28.png";
+import p29_img from "./product_29.png";
+import p30_img from "./product_30.png";
+import p31_img from "./product_31.png";
+import p32_img from "./product_32.png";
+import p33_img from "./product_33.png";
+import p34_img from "./product_34.png";
+import p35_img from "./product_35.png";
+import p36_img from "./product_36.png";
+
+let all_product = [
+ {
+     id:1,
+     name:"Miami 2025 Half Sleeved Tshirt",
+     image:p1_img,
+     category:"clothes",
+     new_price:500.00,
+     old_price:800.50,
+   },
+   {id:2,
+     name:"Miami 2025 Half Sleeved shirt",
+     image:p2_img,
+     new_price:850.00,
+     old_price:1200.50,
+     category:"clothes",
+   },
+   {id:3,
+     name:"Ferrari 2025 Tshirt",
+     image:p3_img,
+     category:"clothes",
+     new_price:600.00,
+     old_price:1000.50,
+   },
+
+   {id:4,
+     name:"Lando Norris Austria Tshirt",
+     image:p4_img,
+     category:"clothes",
+     new_price:600.00,
+     old_price:1500,
+   },
+  {
+    id: 5,
+    name: "Skin Colored Miami GP Half Tshirt",
+    category: "clothes",
+    image: p5_img,
+    new_price: 850.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 6,
+    name: "F2004 Exclusive Model Car",
+    category: "car_Models",
+    image: p6_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 7,
+    name: "James Hunt posters signed by Freddie Hunt",
+    category: "posters",
+    image: p7_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+     id: 8,
+     name: "V6 Mercedes Model",
+     image: p8_img,
+     category:"engine_Models",
+     new_price: 10000.0,
+     old_price: 15000.0,
+  },
+  {
+    id: 9,
+    name: "Checo Perez Half Tshirt Redbull",
+    category: "clothes",
+    image: p9_img,
+    new_price: 850.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 10,
+    name: "RedBull Half Tshirt",
+    category: "clothes",
+    image: p10_img,
+    new_price: 805.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 11,
+    name: "Max Verstappen Hoodie",
+    category: "clothes",
+    image: p11_img,
+    new_price: 850.0,
+    old_price: 1200.5,
+  },
+  
+   {
+      id: 12,
+      name: "RedBull Chained Full Sleeve Jacket",
+      image: p12_img,
+      new_price: 500.0,
+      category:"clothes",
+      old_price: 800.5,
+    },
+  
+  {
+    id: 13,
+    name: "Michael Schumacher Helmet",
+    category: "posters",
+    image: p13_img,
+    new_price: 850.0,
+    old_price: 12000.5,
+  },
+  {
+      id: 14,
+      name: "F2004 Engine Model, V10 for kids and adults",
+      image: p14_img,
+      new_price: 6000.0,
+      old_price: 10000.5,
+      category:"engine_Models",
+  },
+  {
+      id: 15,
+      name: "Ferrari F1 2025 Cap",
+      image: p15_img,
+      category:"clothes",
+      new_price: 500.0,
+      old_price: 800.5,
+  },
+  {
+    id: 16,
+    name: "Max Verstappen Brazil 2024 Win",
+    category: "posters",
+    image: p16_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+      id: 17,
+      name: "RB19 DIY Model Car",
+      image: p17_img,
+      category:"car_Models",
+      new_price: 6000.0,
+      old_price: 10000.5,
+  },
+  {
+    id: 18,
+    name: "Ayrton Senna Brazil Helmet",
+    category: "posters",
+    image: p18_img,
+    new_price: 8599.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 19,
+    name: "20 years of RBR",
+    category: "posters",
+    image: p19_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 20,
+    name: "Fernando Alonso AMR 2023",
+    category: "posters",
+    image: p20_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 21,
+    name: "Sf-25 Model car",
+    category: "car_Models",
+    image: p21_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 22,
+    name: "SF-71H Model Car",
+    category: "car_Models",
+    image: p22_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 23,
+    name: "RB18 Steering Wheel",
+    category: "car_Models",
+    image: p23_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 24,
+    name: "Pirelli Tyres",
+    category: "car_Models",
+    image: p24_img,
+    new_price: 8599.0,
+    old_price: 12099.5,
+  },
+  {
+    id: 25,
+    name: "MCL18",
+    category: "car_Models",
+    image: p25_img,
+    new_price: 8599.0,
+    old_price: 12990.5,
+  },
+  {
+    id: 26,
+    name: "V12 Engine Model",
+    category: "engine_Models",
+    image: p26_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+  {
+    id: 27,
+    name: "Ferrari V10 Engine Model",
+    category: "engine_Models",
+    image: p27_img,
+    new_price: 8500.0,
+    old_price: 12000.5,
+  },
+ {
+    id: 28,
+    name: "Brawn GP 2009 Limited Editions posters",
+    image: p28_img,
+    new_price: 1009.0,
+    old_price: 1509.0,
+    category:"posters",
+  },
+  {
+    id: 29,
+    name: "V6 Engine Model",
+    category: "engine_Models",
+    image: p29_img,
+    new_price: 8599.0,
+    old_price: 12099.5,
+  },
+  {
+    id: 30,
+    name: "RedBull Cap",
+    category: "clothes",
+    image: p30_img,
+    new_price: 859.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 31,
+    name: "Ferrari Long Sleeve Jacket",
+    category: "clothes",
+    image: p31_img,
+    new_price: 8599.0,
+    old_price: 12099.5,
+  },
+  {
+    id: 32,
+    name: "Ferrari Long Sleeved Hoodie",
+    category: "clothes",
+    image: p32_img,
+    new_price: 4000.0,
+    old_price: 4500.5,
+  },
+  {
+    id: 33,
+    name: "Ferrari Half sleeved Tshirt",
+    category: "clothes",
+    image: p33_img,
+    new_price: 805.0,
+    old_price: 1209.5,
+  },
+  {
+    id: 34,
+    name: "F1 Full Sleeved Hoodie",
+    category: "clothes",
+    image: p34_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+     id: 35,
+     name: "Black Miami Half Tshirt",
+     image: p35_img,
+     new_price: 850.0,
+     old_price: 1200.5,
+     category:"clothes",
+  },
+  {
+    id: 36,
+    name: "Aston Martin Long Sleeved Hoodie",
+    category: "clothes",
+    image: p36_img,
+    new_price: 805.0,
+    old_price: 1200.5,
+  },
+];
+
+export default all_product;
---
+++ admin/src/assets/Frontend_Assets/new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "RedBull Chained Full Sleeve acket",
+    image: p1_img,
+    new_price: 500.0,
+    old_price: 800.5,
+  },
+  {
+    id: 35,
+    name: "Black Miami Half Tshirt",
+    image: p2_img,
+    new_price: 850.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 14,
+    name: "F2004 Engine Model, V10 for kids and adults",
+    image: p3_img,
+    new_price: 6000.0,
+    old_price: 10000.5,
+  },
+  {
+    id: 8,
+    name: "V6 Mercedes Model",
+    image: p4_img,
+    new_price: 10000.0,
+    old_price: 15000.0,
+  },
+  {
+    id: 15,
+    name: "Ferrari F1 2025 Cap",
+    image: p5_img,
+    new_price: 500.0,
+    old_price: 800.5,
+  },
+  {
+    id: 2,
+    name: "F1 Miami 2025 Half Shirt",
+    image: p6_img,
+    new_price: 859.0,
+    old_price: 1299.5,
+  },
+  {
+    id: 17,
+    name: "RB19 DIY Model Car",
+    image: p7_img,
+    new_price: 6000.0,
+    old_price: 10000.5,
+  },
+  {
+    id: 28,
+    name: "Brawn GP 2009 Limited Editions Poster",
+    image: p8_img,
+    new_price: 1009.0,
+    old_price: 1509.0,
+  },
+];
+
+export default new_collections;
