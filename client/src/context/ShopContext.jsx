764c155d24c8d207951a14c642018683fb52ed6d
Rishav Paudel <tppaudel2018@gmail.com>
2025-08-02 19:13:29 +0545

    Videos and a bit of shopcontext

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,16 +1,21 @@
-import React,{createContext,useState} from 'react'
-import all_product from '../assets/Frontend_Assets/all_product';
+import React,{createContext,useState,useEffect} from 'react'
 
 export const ShopContext=createContext(null);
    const getDefaultCart =()=>{
         let cart={};
-        for(let index=0;index<all_product.length+1;index++){
+        for(let index=0;index<300+1;index++){
             cart[index]=0;
         }
         return cart;
     }
 const ShopContextProvider=(props)=>{
- const [cartItems,setCartItems]=useState(getDefaultCart());
+    const [all_product,setAll_product]=useState([]);
+    const [cartItems,setCartItems]=useState(getDefaultCart());
+
+    useEffect (()=>{
+        fetch('http://localhost:4000/allproducts').then((response)=>response.json())
+        .then((data)=>setAll_product(data))
+    },[])
     const addToCart=(itemId)=>{
          setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
       
